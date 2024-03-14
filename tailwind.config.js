f40a62bce689dff1ca8154fa680e43afcafdf7b8
tomo-local <shinsi4545@gmail.com>
2024-03-14 22:34:42 +0900

    コンポーネントのパスを変更しました

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
